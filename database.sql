ebaaab49d13d91572924f5f60321e3bfef730895
nguyenvany9a2-alt <nguyenvany9a2@gmail.com>
2026-05-17 12:52:30 +0700

    CNPM-01: Thiet ke database

---
+++ database.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Users (
+    id INT PRIMARY KEY IDENTITY,
+    username NVARCHAR(50),
+    password NVARCHAR(100)
+);
+
+CREATE TABLE Products (
+    id INT PRIMARY KEY IDENTITY,
+    name NVARCHAR(100),
+    price FLOAT,
+    description NVARCHAR(255),
+    image NVARCHAR(255)
+);
+
+CREATE TABLE Orders (
+    id INT PRIMARY KEY IDENTITY,
+    user_id INT,
+    total FLOAT
+);
