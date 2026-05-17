7a44fbe9ba5b89affa194a98b9e7c1281b1dab25
nguyenvany9a2-alt <nguyenvany9a2@gmail.com>
2026-05-17 14:20:14 +0700

    CNPM-03: Homepage UI

---
+++ frontend/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Hệ thống cảnh báo thuốc</h1>
+      <p>Kiểm tra chống chỉ định và tương tác thuốc</p>
+
+      <a href="/input">Nhập thuốc</a>
+    </div>
+  );
+}
