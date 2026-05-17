5ed247e3b8279b11e9eab87322ed0d57d047a7ac
nguyenvany9a2-alt <nguyenvany9a2@gmail.com>
2026-05-17 15:11:51 +0700

    CNPM-06: Drug warning feature

---
+++ frontend/pages/HistoryPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function HistoryPage() {
+  return (
+    <div>
+      <h2>Lịch sử tra cứu</h2>
+      <ul>
+        <li>Paracetamol + Tiểu đường</li>
+      </ul>
+    </div>
+  );
+}
