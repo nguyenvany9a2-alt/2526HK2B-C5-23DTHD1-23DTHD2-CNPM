ba00ed1eb5951c78c65464a607f213c0189c8cb2
nguyenvany9a2-alt <nguyenvany9a2@gmail.com>
2026-05-23 09:02:58 +0700

    CNPM-14: Setup React frontend

---
+++ frontend/src/pages/HistoryPage.jsx
@@ -0,0 +1,7 @@
+export default function HistoryPage() {
+  return (
+    <div>
+      <h1>Lịch sử tra cứu</h1>
+    </div>
+  );
+}
---
+++ frontend/src/pages/InputDiseasePage.jsx
@@ -0,0 +1,7 @@
+export default function InputDiseasePage() {
+  return (
+    <div>
+      <h1>Nhập bệnh nền</h1>
+    </div>
+  );
+}
